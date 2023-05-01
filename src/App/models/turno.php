2b1387cf73fe24954d7f11a74079be13e3cb9b85
Marcos-AS <54771734+Marcos-AS@users.noreply.github.com>
2023-05-01 00:56:16 -0300

    validaciones en turno

--- src/App/models/turno.php
+++ src/App/models/turno.php
@@ -23,24 +23,49 @@ class Turno {
     ];
 
     public function setNombre(string $nombre) {
+        $pattern = '/\d/';
         if (strlen($nombre) > 60) {
             throw new InvalidValueFormatException("El nombre no debe ser mayor a 60 caracteres");
+        } else if (preg_match($pattern, $nombre)) {
+            throw new InvalidValueFormatException("El nombre no debe contener números");
         }
         $this -> fields["nombre"] = $nombre;
     }
 
     public function setApellido(string $apellido) {
+        $pattern = '/\d/';
         if (strlen($apellido) > 60) {
             throw new InvalidValueFormatException("El apellido no debe ser mayor a 60 caracteres");
+        } else if (preg_match($pattern, $nombre)) {
+            throw new InvalidValueFormatException("El nombre no debe contener números");
         }
         $this -> fields["apellido"] = $apellido;
     }
 
     public function setDni(int $dni) {
-        if (strlen(strval($dni)) > 8) {
-            throw new InvalidValueFormatException("El DNI no puede tener más de 8 dígitos");
+        $pattern = '/[a-zA-Z]/';
+        if (strlen(strval($dni)) > 8 || strlen(strval($dni)) < 7) {
+            throw new InvalidValueFormatException("El DNI debe tener 7 u 8 dígitos");
+        } else if (preg_match($pattern,$dni)) {
+            throw new InvalidValueFormatException("El DNI no debe contener letras");
         }
         $this -> fields["dni"] = $dni;
+    }
+
+    public function setfechaNacimiento($fechaNac) {
+        if (!strtotime($fechaNac) || strtotime($fechaNac) > date('Y-m-d')) {
+            throw new InvalidValueFormatException("La fecha debe ser válida");
+        }
+        $this -> fields["fechaNacimiento"] = $fechaNac;
+    }
+
+    public function setEdad(int $edad) {
+        $nac = new DateTime($fechaNac);
+        $hoy = new DateTime();
+        if ($edad != (date_diff($hoy, $nac))) {
+            throw new InvalidValueFormatException("La edad no es acorde a la fecha de nacimiento");
+        }
+        $this -> fields["edad"] = $edad;
     }
 
     public function setEmail(string $email) {
@@ -48,6 +73,28 @@ class Turno {
             throw new InvalidValueFormatException("Formato de email no valido");
         }
         $this -> fields["email"] = $email;
+    }
+
+    public function setTelefono(string $tel) {
+        $pattern = '/[a-zA-Z]/';
+        if (preg_match($pattern, $tel)) {
+            throw new InvalidValueFormatException("Formato de telefono no valido");
+        }
+        $this -> fields["telefono"] = $tel;
+    }
+
+    public function setFecha(string $fecha) {
+        if (!strtotime($fecha) || strtotime($fechaNac) < date('Y-m-d')) {
+            throw new InvalidValueFormatException("La fecha debe ser válida");
+        }
+        $this -> fields["fecha"] = $fecha;
+    }
+
+    public function setHorario(string $hora) {
+        if (!strtotime($hora) || strtotime($hora) < time()) {
+            throw new InvalidValueFormatException("El horario debe ser válido");
+        }
+        $this -> fields["horario"] = $hora;
     }
 
     public function set(array $values) {
