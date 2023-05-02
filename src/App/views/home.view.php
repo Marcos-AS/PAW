d11da7ecc18e47ac4075caa41b932f3f7f50e9fa
leoduville <71992403+leoduville@users.noreply.github.com>
2023-05-02 10:49:57 -0300

    actualizo

--- src/App/views/home.view.php
+++ src/App/views/home.view.php
@@ -14,46 +14,10 @@
     </head>
     <body>
 
-        <header>
-            <a class="icono" href="/"></a>
-            <a class="botonTurno" href="solicitarTurno"> Reserva tu turno</a>
-            <section class="hambur">
-                <h2>Menú</h2>
-                <button class="hamburguesa">
-                    <span></span>
-                    <span></span>
-                    <span></span>
-                </button>
-            </section>
-        </header> 
+        <?php require __DIR__ . '/./parts/header.view.php' ?>
 
         <?php require __DIR__ . '/./parts/nav.view.php' ?>
 
-     <!--  <nav id="menu">
-            <ul>
-                <li class="dropdown">
-                    <a href="#">Institucional<span><i id="desplegable1" class="icono-caretDown" aria-hidden="true"></i></span></a>             
-                    <ul class="dropdown-menu sub-menu">
-                        <li><a href="/institucional/directorio.html">Autoridades</a></li>
-                        <li><a href="/institucional/historia.html">Historia</a></li>
-                        <li><a href="/institucional/mision.html">Misión</a></li>
-                        <li><a href="/institucional/valores.html">Valores</a></li>                
-                    </ul>
-                </li> 
-                <li><a href="/portal-pacientes/login.html">Portal Pacientes</a></li>
-                <li><a href="/profyesp.html">Profesionales y Especialidades</a></li>
-                <li class="dropdown">
-                    <a href="#">Información Útil<span><i id="desplegable2" class="icono-caretDown" aria-hidden="true"></i></span></a>
-                    <ul class="dropdown-menu sub-menu">
-                        <li><a href="/info-util/coberturasmedicas.html">Coberturas médicas</a></li>
-                        <li><a href="/info-util/novedades.html">Novedades</a></li>
-                        <li><a href="/info-util/patologiasytratamientos.html">Patologías y tratamientos</a></li>
-                    </ul>   
-                </li>
-            </ul>
-        </nav>
-    -->
-    
         <section id="sBusqueda">
             <h2>Bienvenido a nuestro sitio oficial</h2>
             <h3>Encontra a tu profesional</h3>
@@ -70,8 +34,8 @@
             </a>
         </section>
         
-    <section class="contenedor">
-        <section class="sMapa">
+        <section class="contenedor">
+            <section class="sMapa">
             <h3>¿Donde encontrarnos?</h3>
             <iframe class="mapa" src="https://www.google.com/maps/embed?pb=!1m14!1m12!1m3!1d13088.495915509864!2d-60.01438485!3d-34.9033381!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!5e0!3m2!1ses-419!2sar!4v1681595424190!5m2!1ses-419!2sar" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
         </section>
@@ -125,31 +89,10 @@
                 la atención de nuestros pacientes.
             </p>
         </section>
-        
-
     </section>
 
-
-        <footer >
-            <ul>
-                <li><h4>Teléfonos</h4></li>
-                <li>
-                    <a href="tel: +5408009999999">0-800-999-9999</a>
-                </li>
-                <li>
-                    <a href="tel: +54123456">123456</a>
-                </li>
-            </ul>
-            <p>Lunes a viernes de 8 a 21 hs.</p>
-            <ul>
-            <li class="redSocial"><a href="https://www.facebook.com.ar" target="_blank"><i class="icono-facebook"  alt="Logo de Facebook"></i></a></li>
-            <li class="redSocial"><a href="https://www.linkedin.com.ar" target="_blank"><i class="icono-linkedIn" alt="Logo de LinkedIn"></i></a></li>
-            <li class="redSocial"><a href="https://www.instagram.com.ar" target="_blank"><i class="icono-instagram" alt="Logo de Instagram"></i></a></li>
-            </ul>
-            <p>Clínica ... Todos los derechos reservados &#169;</p>   
-            <a href="trabajaconnosotros">Trabaja con nosotros</a>
-        </footer>
-
+        <?php require __DIR__ . '/./parts/footer.view.php' ?>
+        
         <script src="assets/scripts/hamburguesa.js"></script>
 
     </body>
--- src/App/views/info-util/novedades.view.php
+++ src/App/views/info-util/novedades.view.php
@@ -13,42 +13,9 @@
 </head>
 <body>
     
-    <header>
-        <a class="icono" href="/"></a>
-        <button class="hamburguesa">
-            <span></span>
-            <span></span>
-            <span></span>
-        </button>
-        <a class="botonTurno" href="../solicitarTurno"> Reserva tu turno</a>
-    </header>  
+    <?php include __DIR__ . '/../parts/header.view.php' ?>
 
     <?php include __DIR__ . '/../parts/nav.view.php' ?>
-
-    <!--  <nav id="menu">
-        <ul>
-            <li class="dropdown">
-                <a href="#">Institucional<span><i id="desplegable1" class="icono-caretDown" aria-hidden="true"></i></span></a>             
-                <ul class="dropdown-menu sub-menu">
-                    <li><a href="/institucional/directorio.html">Autoridades</a></li>
-                    <li><a href="/institucional/historia.html">Historia</a></li>
-                    <li><a href="/institucional/mision.html">Misión</a></li>
-                    <li><a href="/institucional/valores.html">Valores</a></li>                
-                </ul>
-            </li> 
-            <li><a href="/portal-pacientes/login.html">Portal Pacientes</a></li>
-            <li><a href="/profyesp.html">Profesionales y Especialidades</a></li>
-            <li class="dropdown">
-                <a href="#">Información Útil<span><i id="desplegable2" class="icono-caretDown" aria-hidden="true"></i></span></a>
-                <ul class="dropdown-menu sub-menu">
-                    <li><a href="/info-util/coberturasmedicas.html">Coberturas médicas</a></li>
-                    <li><a href="/info-util/novedades.html">Novedades</a></li>
-                    <li><a href="/info-util/patologiasytratamientos.html">Patologías y tratamientos</a></li>
-                </ul>   
-            </li>
-        </ul>
-    </nav>
-    -->
 
     <nav class="breadcrumb">
         <ul>
@@ -108,29 +75,10 @@
             <img src="/assets/imgs/cocaina.jpg" alt="Noticia Granulomatosis">
             <p>La cocaína y la cocaína mezclada con levamisol se usan cada vez más en el Reino Unido y provocan un daño nasal directo significativo además de promover la vasculitis. Nuestros objetivos fueron los siguientes: (1) identificar los principales síntomas y presentación de la vasculitis inducida por cocaína; (2) proporcionar evidencia sobre las mejores prácticas para la investigación y el diagnóstico de vasculitis inducida por cocaína; y (3) analizar los resultados clínicos de los pacientes para comprender el manejo óptimo de la afección.</p>
             <a href="#">Leer más</a>
-          </article>
-          
+          </article>       
     </section>
 
-    <footer >
-        <ul>
-            <li><h4>Teléfonos</h4></li>
-            <li>
-                <a href="tel: +5408009999999">0-800-999-9999</a>
-            </li>
-            <li>
-                <a href="tel: +54123456">123456</a>
-            </li>
-        </ul>
-        <p>Lunes a viernes de 8 a 21 hs.</p>
-        <ul>
-            <li class="redSocial"><a href="https://www.facebook.com.ar" target="_blank"><i class="icono-facebook"  alt="Logo de Facebook"></i></a></li>
-            <li class="redSocial"><a href="https://www.linkedin.com.ar" target="_blank"><i class="icono-linkedIn" alt="Logo de LinkedIn"></i></a></li>
-            <li class="redSocial"><a href="https://www.instagram.com.ar" target="_blank"><i class="icono-instagram" alt="Logo de Instagram"></i></a></li>
-        </ul>
-        <p>Clínica ... Todos los derechos reservados &#169;</p>   
-        <a href="/trabajaconnosotros">Trabaja con nosotros</a>
-    </footer>
+    <?php include __DIR__ . '/../parts/footer.view.php' ?>
 
     <script src="/assets/scripts/hamburguesa.js"></script>
     <script src="/assets/scripts/submenu.js"></script>
